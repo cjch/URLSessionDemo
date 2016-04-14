6cd9b416e68c6854ce168c64bf01899a7f61d967
cjch <350765427@qq.com>
2016-04-14 20:38:01 +0800

    demo init

---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, ‘7.0’
+
+pod 'Mantle', '~> 2.0.6'
+pod 'UIView+Positioning', '~> 1.1'
---
+++ URLSessionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  URLSessionDemo
+//
+//  Created by chenjie on 16/4/14.
+//  Copyright © 2016年 chenjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
